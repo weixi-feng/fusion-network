2aa420557e9b5a60b8cd1632a2889303cdaf9c63
FENGRaymond <ec.thomasfeng@gmail.com>
2019-08-06 13:04:38 -0700

    argument options

--- opt/base_opt.py
+++ opt/base_opt.py
@@ -7,29 +7,22 @@ class BaseOptions():
         self.initialized = False
 
     def initialize(self, parser):
-        parser.add_argument('--dataroot', required=True, default='./datasets/crop_motion_21_0',
+        parser.add_argument('--dataroot', required=False, default='./datasets/crop_motion_21_0',
                             help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
-        parser.add_argument('--name', type=str, default='deblur_physicsgan',
-                            help='name of the experiment. It decides where to store samples and models')
-        parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
-        parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
+        parser.add_argument('--load_model', type=bool, default=False, help='whether to load model')
+        parser.add_argument('--cuda', type=bool, default=True, help='using cuda')
+        parser.add_argument('--save_dir', type=str, default='./checkpoints', help='models are saved here')
         # model parameters
         parser.add_argument('--model', type=str, default='residual_physics',
                             help='chooses which model to use. [residual_physics | two_stream | dehazenet | our_model]')
         parser.add_argument('--init_type', type=str, default='normal',
                             help='network initialization [normal | xavier | kaiming | orthogonal]')
         # dataset parameters
-        parser.add_argument('--serial_batches', action='store_true',
-                            help='if true, takes images in order to make batches, otherwise takes them randomly')
         parser.add_argument('--num_threads', default=4, type=int, help='# threads for loading data')
-        parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
+        parser.add_argument('--batch_size', type=int, default=4, help='input batch size')
         parser.add_argument('--image_size', type=int, default=256, help='scale images to this size')
-        parser.add_argument('--preprocess', type=str, default='resize_and_crop',
-                            help='scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]')
         # additional parameters
-        parser.add_argument('--epoch', type=str, default='latest',
-                            help='which epoch to load? set to latest to use latest cached model')
-        parser.add_argument('--load_iter', type=int, default='0',
+        parser.add_argument('--load_epoch', type=int, default=0,
                             help='which iteration to load? if load_iter > 0, the code will load models by iter_[load_iter]; otherwise, the code will load models by [epoch]')
         self.initialized = True
         return parser
--- opt/train_opt.py
+++ opt/train_opt.py
@@ -9,13 +9,11 @@ class TrainOptions(BaseOptions):
 
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)
-        # visdom and HTML visualization parameters
-        parser.add_argument('--display_freq', type=int, default=400, help='frequency of showing training results on screen')
         # network saving and loading parameters
-        parser.add_argument('--save_latest_freq', type=int, default=5000, help='frequency of saving the latest results')
-        parser.add_argument('--save_epoch_freq', type=int, default=5, help='frequency of saving checkpoints at the end of epochs')
-        parser.add_argument('--continue_train', action='store_true', help='continue training: load the latest model')
-        parser.add_argument('--epoch_count', type=int, default=1, help='the starting epoch count, we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>, ...')
+        parser.add_argument('--exp_id', type=int, default=1, help='experiment id')
+        parser.add_argument('--save_epoch_freq', type=int, default=20, help='frequency of saving checkpoints at the end of epochs')
+        parser.add_argument('--epoch', type=int, default=100,
+                            help='number of epochs to train')
         # training parameters
         parser.add_argument('--niter', type=int, default=100, help='# of iter at starting learning rate')
         parser.add_argument('--niter_decay', type=int, default=100, help='# of iter to linearly decay learning rate to zero')
